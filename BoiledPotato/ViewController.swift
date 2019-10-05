97e6fad49cbc3b4e2bb54973a25b7d5d374200a7
Oziel Perez <OZLperez11@gmail.com>
2019-10-05 13:41:36 -0500

    Initial Commit

---
+++ BoiledPotato/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoiledPotato
+//
+//  Created by Oziel Perez on 10/4/19.
+//  Copyright © 2019 DreamCraft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BoiledPotatoTests/BoiledPotatoTests.swift
@@ -0,0 +1,34 @@
+//
+//  BoiledPotatoTests.swift
+//  BoiledPotatoTests
+//
+//  Created by Oziel Perez on 10/4/19.
+//  Copyright © 2019 DreamCraft. All rights reserved.
+//
+
+import XCTest
+@testable import BoiledPotato
+
+class BoiledPotatoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
