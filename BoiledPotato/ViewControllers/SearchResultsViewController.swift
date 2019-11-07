d5e5c38daad8251bc3458751df8447fccf3779b3
Oziel Perez <OZLperez11@gmail.com>
2019-11-07 12:01:43 -0600

    Modified SearchResultsViewController to observe for changes in Recipe collection to adjust views

--- BoiledPotato/ViewControllers/SearchResultsViewController.swift
+++ BoiledPotato/ViewControllers/SearchResultsViewController.swift
@@ -3,7 +3,8 @@ import UIKit
 class SearchResultsViewController : UIViewController {
     weak var coordinator : Coordinator?
     let layout = SearchResultsViewLayout()
-    let viewModel : SearchResultsViewModel
+    var searchQueryObserver : NSKeyValueObservation?
+    @objc let viewModel : SearchResultsViewModel
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -22,6 +23,52 @@ class SearchResultsViewController : UIViewController {
         layout.recipeCollection.dataSource = self
         layout.recipeCollection.delegate = self
         layout.recipeCollection.register(UIRecipeCard.self, forCellWithReuseIdentifier: UIRecipeCard.id)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // define callback for search query result data, to update UI
+        searchQueryObserver = observe(\.viewModel.queryObservable) { controller, _ in
+            controller.process(resource: controller.viewModel.queryResult)
+        }
+        
+        viewModel.fetchRecipes()
+    }
+    
+    func process(resource: Resource<RecipeSearchQuery>) {
+        switch(resource) {
+            case is Resource<RecipeSearchQuery>.Loading :
+                toggleLoadingIndicators(); break;
+            case is Resource<RecipeSearchQuery>.Success :
+                toggleLoadingIndicators()
+                displaySearchResults(); break;
+            case is Resource<RecipeSearchQuery>.Error :
+                toggleLoadingIndicators()
+                toggleError(message: resource.message!); break;
+            default: break;
+        }
+    }
+    
+    func toggleLoadingIndicators() {
+        layout.placeholders.forEach { placeholder in
+            placeholder.isShimmering = false
+        }
+        
+        layout.placeholderComponent.isHidden = !layout.placeholderComponent.isHidden
+    }
+    
+    func toggleError(message: String) {
+        layout.errorComponent.message.text = message
+        layout.errorComponent.isHidden = false
+    }
+    
+    func displaySearchResults() {
+        let count = viewModel.recipes.count
+        let indexPaths = Array(count - viewModel.maxResultsSize...count - 1).map {
+            IndexPath(item: $0, section: 0)
+        }
+        
+        layout.recipeCollection.insertItems(at: indexPaths)
+        layout.recipeCollection.isHidden = false
     }
     
     @objc func endScene() {
