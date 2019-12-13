46775c49f26774776bf5250ee116cd1d82ee9dc0
Oziel Perez <OZLperez11@gmail.com>
2019-12-13 11:38:41 -0600

    Added method for fetching recipe details

--- BoiledPotato/Repositories/RecipeRepository.swift
+++ BoiledPotato/Repositories/RecipeRepository.swift
@@ -1,13 +1,20 @@
 import Alamofire
 
 class RecipeRepository {
+    // a short type alias for callbacks from viewmodel, which accept a data Resource
+    typealias Handler<T> = RestApiService.CompleteHandler<T>
+    
     private let apiService: RestApiService
     
     init(restApiService: RestApiService) {
         apiService = restApiService
     }
     
-    func searchRecipes(queryData: Parameters, onComplete handler: @escaping RestApiService.CompleteHandler) {
+    func searchRecipes(queryData: Parameters, onComplete handler: @escaping Handler<RecipeSearchQuery>) {
         apiService.getRecipes(parameters: queryData, onCompletion: handler)
+    }
+    
+    func getRecipe(byId id: CLong, onComplete handler: @escaping Handler<Recipe>) {
+        apiService.getRecipe(byId: id, onCompletion: handler)
     }
 }
