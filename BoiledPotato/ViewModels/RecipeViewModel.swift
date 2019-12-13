6d22a7c18d2ef6a307c450dbc2ecbe20b55d7d18
Oziel Perez <OZLperez11@gmail.com>
2019-12-13 11:39:19 -0600

    Added method for fetching missing recipe details

--- BoiledPotato/ViewModels/RecipeViewModel.swift
+++ BoiledPotato/ViewModels/RecipeViewModel.swift
@@ -1,13 +1,27 @@
 import Foundation
 
-@objc class RecipeViewModel : NSObject {
+class RecipeViewModel : NSObject {
     var recipe : Recipe!
     let repository : RecipeRepository
+    var queryResult : Resource<Recipe> = Resource.Loading()
     
     // observable property to check if queryResult has changed
     @objc dynamic var queryObservable : Bool = false
     
     init(repository: RecipeRepository) {
         self.repository = repository
+    }
+    
+    func fetchRecipeDetails() {
+        repository.getRecipe(byId: recipe.id) { resource in
+            if resource is Resource.Success {
+                self.recipe.servings = resource.data!.servings
+                self.recipe.ingredients = resource.data!.ingredients
+                self.recipe.instructions = resource.data!.instructions
+            }
+            
+            self.queryResult = resource
+            self.queryObservable = !self.queryObservable
+        }
     }
 }
