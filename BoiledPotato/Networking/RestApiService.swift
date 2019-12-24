870f899e277793baa7caacf561f6c9f882552d63
Oziel Perez <OZLperez11@gmail.com>
2019-12-24 01:43:37 -0600

    Refactored RestAPIService to use PromiseKit

--- BoiledPotato/Networking/RestApiService.swift
+++ BoiledPotato/Networking/RestApiService.swift
@@ -1,4 +1,5 @@
 import Alamofire
+import PromiseKit
 
 class RestApiService {
     typealias CompleteHandler<T> = (Resource<T>) -> Void
@@ -14,26 +15,30 @@ class RestApiService {
         ]
     }
     
-    private func createResource<T>(response: DataResponse<T, AFError>) -> Resource<T> {
+    private func resolvePromise<T>(with response: DataResponse<T, AFError>, and resolver: Resolver<T>) {
         switch response.result {
-            case .success: return Resource.Success(response.value!)
-            case .failure: return Resource.Error(response.error!.underlyingError?.localizedDescription ?? "")
+            case .success: resolver.fulfill(response.value!)
+            case .failure: resolver.reject(response.error!.underlyingError!)
         }
     }
     
-    func getRecipes(parameters: Parameters, onCompletion handler: @escaping CompleteHandler<RecipeSearchQuery>) {
-        AF
-        .request(baseURL + "/recipes/search", parameters: parameters, headers: headers)
-        .validate().responseDecodable(of: RecipeSearchQuery.self) { response in
-            handler(self.createResource(response: response))
+    func getRecipes(parameters: Parameters) -> Promise<RecipeSearchQuery> {
+        return Promise { resolver in
+            AF
+            .request(baseURL + "/recipes/search", parameters: parameters, headers: headers)
+            .validate().responseDecodable(of: RecipeSearchQuery.self) { response in
+                self.resolvePromise(with: response, and: resolver)
+            }
         }
     }
     
-    func getRecipe(byId id: CLong, onCompletion handler: @escaping CompleteHandler<Recipe>) {
-        AF
-        .request(baseURL + "/recipes/\(id)/information", headers: headers)
-        .validate().responseDecodable(of: Recipe.self) { response in
-            handler(self.createResource(response: response))
+    func getRecipe(byId id: CLong) -> Promise<Recipe> {
+        return Promise { resolver in
+            AF
+            .request(baseURL + "/recipes/\(id)/information", headers: headers)
+            .validate().responseDecodable(of: Recipe.self) { response in
+                self.resolvePromise(with: response, and: resolver)
+            }
         }
     }
 }
