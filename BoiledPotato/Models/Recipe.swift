efe0f95e6824f4f25c41d9174b481e23ef1961e6
Oziel Perez <OZLperez11@gmail.com>
2019-12-24 00:53:14 -0600

    Added Codable protocol and methods for SQLite compatibility

--- BoiledPotato/Models/Recipe.swift
+++ BoiledPotato/Models/Recipe.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SQLite
 
 struct Recipe {
     var id: CLong = 0
@@ -12,7 +13,7 @@ struct Recipe {
     
     init() {}
     
-    init(id: CLong, name: String, prepMinutes: Int, image: String, ingredients: [String]?, instructions: [String]?, servings: Int = 0) {
+    init(id: CLong, name: String, prepMinutes: Int, image: String, servings: Int = 0, ingredients: [String]?, instructions: [String]?) {
         self.id = id
         self.name = name
         self.prepMinutes = prepMinutes
@@ -23,8 +24,8 @@ struct Recipe {
     }
 }
 
-extension Recipe : Decodable {
-    enum CodingKeys : String, CodingKey {
+extension Recipe : Codable {
+    enum JSONKeys : String, CodingKey {
         case id = "id"
         case name = "title"
         case prepMinutes = "readyInMinutes"
@@ -34,13 +35,19 @@ extension Recipe : Decodable {
         case servings = "servings"
     }
     
+    enum SQLiteKeys : String, CodingKey {
+        case id = "ID"
+        case name = "Name"
+        case prepMinutes = "PrepMinutes"
+        case imageFileName = "ImageFileName"
+        case servings = "Servings"
+        case ingredients = "Ingredients"
+        case instructions = "Instructions"
+    }
+    
+    /** for decoding data from JSON http request data */
     init(from decoder: Decoder) throws {
-        let container     = try decoder.container(keyedBy: CodingKeys.self)
-        let id            = try container.decode(CLong.self, forKey: .id)
-        let name          = try container.decode(String.self, forKey: .name)
-        let prepMinutes   = try container.decode(Int.self, forKey: .prepMinutes)
-        let imageFileName = try container.decode(String.self, forKey: .imageFileName)
-        let servings      = try container.decode(Int.self, forKey: .servings)
+        let container = try decoder.container(keyedBy: JSONKeys.self)
         
         // If recipe object has ingredient and instruction data, decode it according to the
         // structs provided at the bottom of the file
@@ -53,14 +60,52 @@ extension Recipe : Decodable {
         let instructions = instructionsMetadata?.flatMap { $0.steps.map { $0.step } }
         
         self.init(
-            id: id,
-            name: name,
-            prepMinutes: prepMinutes,
-            image: imageFileName,
-            ingredients: ingredients,
-            instructions: instructions,
-            servings: servings
+            id:           try container.decode(CLong.self, forKey: .id),
+            name:         try container.decode(String.self, forKey: .name),
+            prepMinutes:  try container.decode(Int.self, forKey: .prepMinutes),
+            image:        try container.decode(String.self, forKey: .imageFileName),
+            servings:     try container.decode(Int.self, forKey: .servings),
+            ingredients:  ingredients,
+            instructions: instructions
         )
+    }
+    
+    /** for decoding data from SQLite */
+    init(from decoder: Decoder, madeBySQLite: Bool) throws {
+        let container = try decoder.container(keyedBy: SQLiteKeys.self)
+        
+        let ingredients  = try container.decode(String.self, forKey: .ingredients)
+        let instructions = try container.decode(String.self, forKey: .instructions)
+        
+        self.init(
+            id:           try container.decode(CLong.self, forKey: .id),
+            name:         try container.decode(String.self, forKey: .name),
+            prepMinutes:  try container.decode(Int.self, forKey: .prepMinutes),
+            image:        try container.decode(String.self, forKey: .imageFileName),
+            servings:     try container.decode(Int.self, forKey: .servings),
+            ingredients:  ingredients == "" ? ingredients.components(separatedBy: "#!") : nil,
+            instructions: instructions == "" ? instructions.components(separatedBy: "#!") : nil
+        )
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: SQLiteKeys.self)
+        try container.encode(Int(id), forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(prepMinutes, forKey: .prepMinutes)
+        try container.encode(imageFileName, forKey: .imageFileName)
+        try container.encode(servings, forKey: .servings)
+        
+        let reducer = { result, item in
+            // combine all strings into one, delimited by a substring
+            return result + item + "#!"
+        }
+        
+        let ingredients = self.ingredients?.reduce("", reducer) ?? ""
+        let instructions = self.instructions?.reduce("", reducer) ?? ""
+        
+        try container.encode(ingredients, forKey: .ingredients)
+        try container.encode(instructions, forKey: .instructions)
     }
 }
 
--- BoiledPotato/Models/RecipeSearchQuery.swift
+++ BoiledPotato/Models/RecipeSearchQuery.swift
@@ -1,3 +1,6 @@
+import SQLite
+import PromiseKit
+
 struct RecipeSearchQuery {
     var id : CLong = 0
     var searchKeywords : String = ""
@@ -5,6 +8,12 @@ struct RecipeSearchQuery {
     var totalResults : Int = 0
     var expires : CLong = 0
     var recipes : [Recipe]?
+    
+    var isStale : Bool {
+        get { return Double(expires) > Date().timeIntervalSince1970 * 1000 }
+    }
+    
+    init() {} // initiate with defaults
     
     init(id: CLong, keywords: String, cuisine: String, totalResults: Int, expires: CLong) {
         self.id = id
@@ -15,17 +24,52 @@ struct RecipeSearchQuery {
     }
 }
 
-extension RecipeSearchQuery : Decodable {
-    enum CodingKeys : String, CodingKey {
+extension RecipeSearchQuery : Codable {
+    // for decoding from JSON
+    enum JSONKeys : String, CodingKey {
         case totalResults = "totalResults"
         case expires = "expires"
         case recipes = "results"
     }
     
+    // for encoding to SQLite
+    enum SQLiteKeys: String, CodingKey {
+        case id = "ID"
+        case searchKeywords = "Keywords"
+        case cuisine = "Cuisine"
+        case totalResults = "ResultAmount"
+        case expires = "ExpirationTime"
+    }
+    
+    /** for instantiating from JSON http request data */
     init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.totalResults = try container.decode(Int.self, forKey: .totalResults)
-        self.expires = try container.decode(Int.self, forKey: .expires)
-        self.recipes = try container.decode([Recipe].self, forKey: .recipes)
+        let container = try decoder.container(keyedBy: JSONKeys.self)
+        totalResults  = try container.decode(Int.self, forKey: .totalResults)
+        expires       = try container.decode(Int.self, forKey: .expires)
+        recipes       = try container.decode([Recipe].self, forKey: .recipes)
+    }
+    
+    /** for instantiating from SQLite Row object */
+    init(from decoder: Decoder, madeBySQLite: Bool) throws {
+        let container  = try decoder.container(keyedBy: SQLiteKeys.self)
+        searchKeywords = try container.decode(String.self, forKey: .searchKeywords)
+        cuisine        = try container.decode(String.self, forKey: .cuisine)
+        totalResults   = try container.decode(Int.self, forKey: .totalResults)
+        expires        = try container.decode(Int.self, forKey: .expires)
+    }
+    
+    /** for encoding to SQLite database */
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: SQLiteKeys.self)
+        
+        if id != 0 {
+            // save id to database if
+            try container.encode(Int(id), forKey: .id)
+        }
+        
+        try container.encode(searchKeywords, forKey: .searchKeywords)
+        try container.encode(cuisine, forKey: .cuisine)
+        try container.encode(totalResults, forKey: .totalResults)
+        try container.encode(expires, forKey: .expires)
     }
 }
