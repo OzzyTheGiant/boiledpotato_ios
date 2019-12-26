e95cc3cd94871467fb1598501d4b0b86d4ed1fe4
Oziel Perez <OZLperez11@gmail.com>
2019-12-26 09:57:07 -0600

    Added LocalizedError protocol for printing user friendly message

--- BoiledPotato/Errors/DataError.swift
+++ BoiledPotato/Errors/DataError.swift
@@ -2,8 +2,10 @@ import Foundation
 
 enum DataError : Error {
     case noResultsFound
-    
-    var localizedDescription: String {
+}
+
+extension DataError : LocalizedError {
+    public var errorDescription: String? {
         get {
             switch(self) {
                 case .noResultsFound: return NSLocalizedString("NO_DATA_ERROR", comment: "")
