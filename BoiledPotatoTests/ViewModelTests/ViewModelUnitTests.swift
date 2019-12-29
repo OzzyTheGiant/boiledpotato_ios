8a8e7d2755bcd8001ba81c5f742ae702128a7de7
Oziel Perez <OZLperez11@gmail.com>
2019-12-29 14:46:49 -0600

    Added unit tests for RecipeViewModel

--- BoiledPotatoTests/ViewModelTests/ViewModelUnitTests.swift
+++ BoiledPotatoTests/ViewModelTests/ViewModelUnitTests.swift
@@ -29,9 +29,59 @@ class SearchResultViewModelUnitTest : XCTestCase {
     }
 }
 
+class RecipeViewModelUnitTest : XCTestCase {
+    fileprivate var repositoryMock : RecipeRepositoryMock?
+    var viewModel : RecipeViewModel?
+    let timeoutSeconds : Double = 10
+    
+    override func setUp() {
+        repositoryMock = RecipeRepositoryMock(restApiService: RestAPIServiceMock(apiURL: "", apiKey: ""), recipeDAO: RecipeDAOMock())
+        viewModel = RecipeViewModel(repository: repositoryMock!)
+        viewModel!.recipe = repositoryMock!.recipe
+    }
+    
+    func testViewModelGetsRecipeDetails() {
+        viewModel!.fetchRecipeDetails()
+        XCTAssert(viewModel!.queryResult is Resource.Success)
+        XCTAssertEqual(viewModel!.recipe.name, viewModel!.queryResult.data!.name)
+        XCTAssert(viewModel!.queryObservable)
+    }
+    
+    func testViewModelGetsErrorResource() {
+        viewModel!.recipe.name = "Burger"
+        viewModel!.fetchRecipeDetails()
+        XCTAssert(viewModel!.queryResult is Resource.Error)
+        XCTAssert(viewModel!.queryObservable)
+    }
+    
+    func testViewModelDoesNothingIfRecipeNotFavorite() {
+        viewModel!.checkIfRecipeIsFavorite()
+        XCTAssert(viewModel!.favoriteResult is Resource.Loading)
+        XCTAssert(!viewModel!.recipe.isFavorite)
+        XCTAssert(!viewModel!.favoriteObservable)
+    }
+    
+    func testViewModelUpdatesFavoriteStatusOnFirstCheck() {
+        viewModel!.recipe.id = 1
+        viewModel!.checkIfRecipeIsFavorite()
+        XCTAssert(viewModel!.favoriteResult is Resource.Success)
+        XCTAssert(viewModel!.favoriteResult.data!)
+        XCTAssert(viewModel!.favoriteObservable)
+        XCTAssert(viewModel!.recipe.isFavorite)
+    }
+    
+    func testViewModelTogglesFavoriteStatus() {
+        viewModel!.toggleRecipeAsFavorite()
+        XCTAssert(viewModel!.favoriteResult is Resource.Success)
+        XCTAssert(viewModel!.favoriteResult.data!)
+        XCTAssert(viewModel!.favoriteObservable)
+        XCTAssert(viewModel!.recipe.isFavorite)
+    }
+}
+
 fileprivate class RecipeRepositoryMock : RecipeRepository {
     var query = TestData.sampleQuery
-    let recipe = TestData.sampleRecipe
+    var recipe = TestData.sampleRecipe
     
     override init(restApiService: RestApiService, recipeDAO: RecipeDAO) {
         super.init(restApiService: restApiService, recipeDAO: recipeDAO)
@@ -42,5 +92,21 @@ fileprivate class RecipeRepositoryMock : RecipeRepository {
         if queryData["query"] as! String == "Beef" {
             return complete(Resource.Error("Test Error"))
         };  complete(Resource.Success(query))
+    }
+    
+    override func getDetails(for recipe: Recipe, onComplete complete: @escaping RecipeRepository.Handler<Recipe>) {
+        if recipe.name == "Burger" {
+            return complete(Resource.Error("Test Error"))
+        };  complete(Resource.Success(recipe))
+    }
+    
+    override func checkIsFavorite(recipeId: CLong, onComplete complete: @escaping RecipeRepository.Handler<Bool>) {
+        if recipeId == 1 {
+            return complete(Resource.Success(true))
+        }; complete(Resource.Success(false))
+    }
+    
+    override func toggleFavorite(on: Bool, recipeId: CLong, onComplete complete: @escaping RecipeRepository.Handler<Bool>) {
+        complete(Resource.Success(on))
     }
 }
